fbc87ad4e919577f03c7b574011b047d58e2b3af
GabriellMiran <gabrielmiran00@gmail.com>
2025-06-23 01:44:52 -0400

    Erros

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.100.5:3000/api",
+  baseURL: "http://160.20.22.99:5151/api",
 });
-/* http://160.20.22.99:3591/api */
+/* http://160.20.22.99:5151/api */
 export default api;
