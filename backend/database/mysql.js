5c15356e38c7480e114730751e7c978018215702
GabriellMiran <gabrielmiran00@gmail.com>
2025-06-29 01:51:11 -0400

    Update mysql.js

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '..', '.env'
 console.log(process.env.DB_USER);
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306, 
+  port: process.env.DB_PORT || 3000, 
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
