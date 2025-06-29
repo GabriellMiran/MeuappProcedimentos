2c0a4fedb2a408e4f7c278ac6e3239325b55bec3
GabriellMiran <gabrielmiran00@gmail.com>
2025-06-29 01:49:58 -0400

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://160.20.22.99:5151/api",
+  baseURL: "http://localhost:3000/api",
 });
-/* http://160.20.22.99:5151/api */
+/* coloque o seu IP */
 export default api;
